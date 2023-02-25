961d2346324fa7a4bd9a9c843b6e8e2f30a553e7
samirahmad5445 <samirahmad5445@gmail.com>
2023-02-25 16:01:32 +0200

    change the icons image with srcset

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const icons = document.querySelectorAll(".icon");
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    console.log(icon);
+  });
+});
